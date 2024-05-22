ab6f369987b9edb262de0daac95391150dc58c7e
kelly momanyi <kellymomanyi80@gmail.com>
2024-05-23 01:55:09 +0300

    picture updatre

--- scripts.js
+++ scripts.js
@@ -3,4 +3,3 @@ console.log("Welcome to Richie & Kelly's Portfolio!");
 function changeBackgroundColor(element) {
     element.style.backgroundColor = 'lightblue'; // Change to your desired color
 }
-const currentURL = window.location.href;
